cf8ef634fffd4022f8e9b2e91fc136c4aaf5af61
Timothy Lee <timothylee.us@gmail.com>
2019-10-21 18:23:12 -0700

    Delimiter Header

--- Delimiter.java
+++ Delimiter.java
@@ -1,3 +1,18 @@
+/**
+ * Delimiter provides information about how the file read/write should work.
+ *
+ * Copyright (C) 2019 Timothy Lee - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the GNU General Public License v3.0.
+ *
+ * You should have received a copy of the GNU General
+ * Public License v3.0 with this file. If not, please
+ * contact: timothyl@berkeley.edu, or visit:
+ * https://github.com/LTimothy/DoubleEntry
+ *
+ * @author: Timothy Lee (https://timothylee.us)
+ */
+
 class Delimiter {
 	private String delimiter;
 	private String saveDelimiter;
